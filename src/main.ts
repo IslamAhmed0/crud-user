627c9e0ccbb141989e70bd57d21bbf43b9dc4ff5
IslamAhmed0 <ea43533@gmail.com>
2022-09-11 21:46:11 +0200

    ...

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
